edbeae67a9f8ff0bac6a93b02f638b0851da0b37
John Moberg <john@moberg.ai>
2022-12-02 13:59:46 +0100

    New model and better prompt

--- main.py
+++ main.py
@@ -16,26 +16,25 @@ re_acronym = re.compile(r"\b[A-ZÅÄÖÜß0-9]{2,}\b")
 
 gpt3_contex = """List of acronyms
 MAKE SURE THAT THE LETTERS MATCH EXACTLY!
-
-NYOB | None of Your Business
-OFC | Of Course
-OMG | Oh My God
-PANS | Pretty Awesome New Stuff
-PHAT | Pretty, Hot, And Tempting
-POS | Parents Over Shoulder
-ROFL | Rolling On the Floor Laughing
-SMH | Shaking My Head
-TTYL | Talk To You Later
-YOLO | You Only Live Once
-AXB | Acronym eXplanation Bot
-WTH | What The Heck
-ROFLMAOSHICAD | Rolling On Floor Laughing My Ass Off So Hard I Choke And Die
+N Y O B | None of Your Business
+O F C | Of Course
+O M G | Oh My God
+P A N S | Pretty Awesome New Stuff
+P H A T | Pretty, Hot, And Tempting
+P O S | Parents Over Shoulder
+R O F L | Rolling On the Floor Laughing
+S M H | Shaking My Head
+T T Y L | Talk To You Later
+Y O L O | You Only Live Once
+A X B | Acronym eXplanation Bot
+W T H | What The Heck
+R O F L M A O S H I C A D | Rolling On Floor Laughing My Ass Off So Hard I Choke And Die
 """
 
 
 def gpt_3_define_acronym(acronym):
-    url = "https://api.openai.com/v1/engines/text-davinci-002/completions"
-    prompt = gpt3_contex + acronym + " |"
+    url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+    prompt = gpt3_contex + " ".join(list(acronym)) + " |"
     payload = {"prompt": prompt, "max_tokens": 3 * len(acronym), "temperature": 0.0}
     response = requests.post(
         url,
